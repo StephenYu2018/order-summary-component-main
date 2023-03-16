71e958ef1a3ef7c2c3158efc49191c535cfa7739
StephenYu2018 <stephenyu8021@gmail.com>
2023-03-16 16:13:41 -0400

    add white color

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ module.exports = {
       "bright-blue": "hsl(245, 75%, 52%)",
       "very-pale-blue": "hsl(225, 100%, 98%)",
       "desaturated-blue": "hsl(224, 23%, 55%)",
-      "dark-blue": "hsl(223, 47%, 23%)"
+      "dark-blue": "hsl(223, 47%, 23%)",
+      "white": "#FFFFFF",
     },
     fontFamily: {
       display: ["\'Red Hat Display\'", "sans-serif"],
