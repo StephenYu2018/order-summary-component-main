135038098a5f101aac222eb308a388d31366e5da
StephenYu2018 <stephenyu8021@gmail.com>
2023-03-15 22:54:22 -0400

    setup TailwindCSS framework

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    colors: {
+      "pale-blue": "hsl(225, 100%, 94%)",
+      "bright-blue": "hsl(245, 75%, 52%)",
+      "very-pale-blue": "hsl(225, 100%, 98%)",
+      "desaturated-blue": "hsl(224, 23%, 55%)",
+      "dark-blue": "hsl(223, 47%, 23%)"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
