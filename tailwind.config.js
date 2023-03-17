fdce87e85d2fac30aee163e9e61a1c5994f04964
StephenYu2018 <stephenyu8021@gmail.com>
2023-03-17 00:16:54 -0400

    add hover states

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     colors: {
       "pale-blue": "hsl(225, 100%, 94%)",
       "bright-blue": "hsl(245, 75%, 52%)",
+      "active-bright-blue": "hsl(245, 75%, 65%)",
       "very-pale-blue": "hsl(225, 100%, 98%)",
       "desaturated-blue": "hsl(224, 23%, 55%)",
       "dark-blue": "hsl(223, 47%, 23%)",
